8c4ca385488b1bb3fbddd6c731f569c153b77bc8
mikegivens-webdesign <givensm095@gmail.com>
2026-03-29 16:29:52 -0500

    sadagdffdsg

---
+++ intro.js
@@ -0,0 +1,15 @@
+
+let comic_image;
+
+function preload() {
+    sprite_sheet = loadSpriteSheet('./assets/dog_spritesheet.png', 296, 355, 2)
+    dog_animation = loadAnimation()
+}
+
+function setup() {
+    createCanvas(400,400)
+}
+
+function draw() {
+    clear()
+}
